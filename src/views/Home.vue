251bd17d8903d1a0b3e67bf2611513dc3807d533
Fernando <zleckzxd@gmail.com>
2021-10-24 02:48:08 -0300

    cleaned default vue views

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <hello-world />
+  <v-container>
+    <v-row justify=center>
+      <v-col align=center>
+        <h1>Home view</h1>
+      </v-col> 
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
-
 export default {
   name: "Home",
-
-  components: {
-    HelloWorld,
-  },
 };
 </script>
