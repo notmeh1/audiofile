c97aa5fd6265eb873c7f705142723d8356da0616
Fernando <zleckzxd@gmail.com>
2021-10-30 03:39:19 -0300

    added mobile view + details

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <Navbar/>
     <v-main>
       <router-view />
     </v-main>
@@ -7,11 +8,19 @@
 </template>
 
 <script>
+import Navbar from "./components/navbar.vue"
 export default {
   name: "App",
 
   data: () => ({
     //
   }),
+  components: {
+    Navbar,
+  }
 };
 </script>
+
+<style lang="scss">
+@import "./sass/main.scss";
+</style>
