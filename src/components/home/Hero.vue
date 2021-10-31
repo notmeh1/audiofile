4196a78a5ea88d8f3f4feea08f060a22162c758f
cgonzalezsotelo <cgonzalezsotelo@gmail.com>
2021-10-30 22:25:34 -0300

    WIP Hero Home

---
+++ src/components/home/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col class="hero">
+        <h1 class="hero__title">Lorem ipsum dolor sit amet, consectetur</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat
+          suscipit porta odio justo, egestas. Auctor porttitor sed augue
+          elementum interdum.
+        </p>
+      </v-col>
+      <v-col>
+        <img src="" alt="" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss">
+.hero {
+  background-color: #f0f6ff;
+  &__title {
+    font-size: 60px;
+    line-height: 60px;
+    text-align: center;
+    color: #4a2aa7;
+    font-weight: 900;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,23 @@
 <template>
   <v-container>
-    <v-row justify=center>
-      <v-col align=center>
+    <v-row justify="center">
+      <v-col align="center">
         <h1>Home view</h1>
-      </v-col> 
+      </v-col>
+      <Hero />
     </v-row>
   </v-container>
 </template>
 
 <script>
+import Hero from "../components/home/Hero.vue";
 export default {
   name: "Home",
+  components: { Hero },
 };
 </script>
+<style lang="scss">
+* {
+  font-family: "Roboto", sans-serif;
+}
+</style>
