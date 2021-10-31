7717a7cc151be3780594716d19d2d8ff90496195
cgonzalezsotelo <cgonzalezsotelo@gmail.com>
2021-10-30 23:28:30 -0300

    hero mobile

--- src/components/home/Hero.vue
+++ src/components/home/Hero.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col class="hero">
-        <h1 class="hero__title">Lorem ipsum dolor sit amet, consectetur</h1>
-        <p>
+    <v-row class="hero mx-1">
+      <v-col class="hero__bg">
+        <h1 class="hero__title pa-4">
+          Lorem ipsum dolor sit amet, consectetur
+        </h1>
+        <p class="hero__text px-4 py-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat
           suscipit porta odio justo, egestas. Auctor porttitor sed augue
           elementum interdum.
         </p>
+        <Search class="pb-8" />
       </v-col>
       <v-col>
         <img src="" alt="" />
@@ -16,17 +19,31 @@
   </v-container>
 </template>
 <script>
-export default {};
+import Search from "./Search.vue";
+export default {
+  components: {
+    Search,
+  },
+};
 </script>
 <style lang="scss">
 .hero {
-  background-color: #f0f6ff;
+  display: flex;
+  flex-direction: column;
+  &__bg {
+    background-color: #f0f6ff;
+    border-radius: 0 0 50px 50px;
+  }
   &__title {
-    font-size: 60px;
+    font-size: 50px;
     line-height: 60px;
     text-align: center;
     color: #4a2aa7;
     font-weight: 900;
+  }
+  &__text {
+    text-align: center;
+    line-height: 30px;
   }
 }
 </style>
