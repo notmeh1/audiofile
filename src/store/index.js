410668d10f11510f66c28a5a64023209544f7a94
cgonzalezsotelo <cgonzalezsotelo@gmail.com>
2021-11-06 22:45:45 -0300

    cards resenas desde firestore

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 import { sessionModule } from "./modules/session";
+import { resenasModule } from "./modules/resenas";
 
 export default new Vuex.Store({
   state: {},
@@ -11,5 +12,6 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     session: sessionModule,
+    resenas: resenasModule,
   },
 });
--- src/views/Resenas.vue
+++ src/views/Resenas.vue
@@ -12,8 +12,13 @@
       <v-row>
         <v-col>
           <FiltrosResenas />
-          <CardResenas />
+          <v-col v-for="resena in $store.state.resenas.data" :key="resena.id">
+            <CardResenas :value="resena" />
+          </v-col>
         </v-col>
+      </v-row>
+      <v-row class="justify-center">
+        <v-pagination v-model="page" :length="6" color="#4A2AA7"></v-pagination>
       </v-row>
     </v-container>
   </div>
@@ -22,8 +27,13 @@
 <script>
 import FiltrosResenas from "../components/resenas/FiltrosResenas.vue";
 import CardResenas from "../components/resenas/CardResenas.vue";
+import Store from "../store";
 export default {
   components: { FiltrosResenas, CardResenas },
+  beforeRouteEnter(to, from, next) {
+    Store.dispatch("resenas/traerTodasLasResenas");
+    next();
+  },
 };
 </script>
 
