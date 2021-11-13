20724dffc6bdb60f80341b52fbac7d9a23cbc57e
cgonzalezsotelo <cgonzalezsotelo@gmail.com>
2021-11-13 03:58:43 -0300

    WIP edit resena admin rol

--- src/views/EditarResena.vue
+++ src/views/EditarResena.vue
@@ -1,12 +1,123 @@
-.
 <template>
   <div>
-    <h1>view editar reseña</h1>
+    <v-container fluid>
+      <v-row class="agregarResenas justify-center">
+        <v-col cols="12" md="8">
+          <h2 class="agregarResenas__title my-5">
+            Busca una canción y agrega una reseña
+          </h2>
+          <v-form class="my-5" ref="formResena" @submit.prevent="crearResena">
+            <v-text-field
+              @input="$store.dispatch('spotify/fetchSongResult', searchInput)"
+              :value="formResena.songId"
+              outlined
+              append-icon="mdi-playlist-music"
+              label="Nombre canción"
+              color="#4a2aa7"
+              v-model="searchInput"
+              :rules="[required]"
+            >
+            </v-text-field>
+
+            <v-card
+              @click="selectSong(song)"
+              class="pointer d-flex my-1"
+              v-for="song in songResult"
+              :key="song.id"
+              absolute
+            >
+              <v-img
+                class="mr-auto my-1"
+                :src="song.album.images[0].url"
+                max-width="10%"
+                width="64px"
+                height="64px"
+                contain
+              />
+              <v-card-title
+                >{{ song.name }} - {{ song.artists[0].name }}</v-card-title
+              >
+            </v-card>
+            <v-rating
+              v-model="formResena.valoracion"
+              color="primary"
+              empty-icon="mdi-star-outline"
+              full-icon="mdi-star"
+              half-icon="mdi-star-halffull"
+              hover
+              length="5"
+              size="24"
+            ></v-rating>
+
+            <v-textarea
+              outlined
+              append-icon="mdi-file-document-edit-outline"
+              name="input-7-4"
+              value="Reseña"
+              label="Reseña"
+              color="#4a2aa7"
+              v-model="formResena.resena"
+              :rules="[required]"
+            ></v-textarea>
+            <v-btn class="agregarResenas__btn" color="#4a2aa7" type="submit"
+              ><v-icon>mdi-pencil</v-icon>Reseñar</v-btn
+            >
+          </v-form>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+import Firebase from "firebase";
+import store from "../store/index";
+import { mapState } from "vuex";
+
+export default {
+  data: () => ({
+    searchInput: null,
+    formResena: {
+      songId: "",
+      resena: "",
+      valoracion: null,
+      comentarios: {},
+    },
+  }),
+  computed: {
+    ...mapState({
+      songResult: (state) => state.spotify.songResult,
+    }),
+  },
+  methods: {
+    crearResena() {
+      if (this.$refs.formResena.validate()) {
+        Firebase.firestore()
+          .collection("foros")
+          .add(this.formResena)
+          .then(() => {
+            this.$store.dispatch("spotify/cleanSearch");
+            this.$router.push("/resenas");
+          })
+          .catch(() => {});
+      }
+    },
+    required(value) {
+      return !!value || "Este campo es obligatorio";
+    },
+    selectSong(song) {
+      console.log(song);
+      store.dispatch("spotify/filterSongId", song.id);
+      const form = this.formResena;
+      form.songId = song.id;
+      form.songName = song.name;
+      form.songImg = song.album.images[0].url;
+      form.songArtistOne = song.artists[0].name;
+      form.album = song.album.name;
+      console.log(this.formResena);
+    },
+  },
+};
 </script>
 
 <style></style>
